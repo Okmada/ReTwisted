aec68d1b029647c73ae93325cdb2f470720864af
Okmada <adamko.deak@gmail.com>
2024-06-21 23:44:33 +0200

    Replaced pywin32 with ctypes

--- src/roblox.py
+++ src/roblox.py
@@ -4,11 +4,41 @@ import time
 
 import cv2
 import numpy as np
-import win32con
-import win32gui
-import win32ui
 
-DESKTOP = win32gui.GetDesktopWindow()
+user32 = ctypes.windll.user32
+gdi32 = ctypes.windll.gdi32
+
+WM_CLOSE = 0x10
+
+SRCCOPY = 0xCC0020
+
+class BitmapInfoHeader(ctypes.Structure):
+    _fields_ = [("biSize", ctypes.c_ulong),
+                ("biWidth", ctypes.c_long),
+                ("biHeight", ctypes.c_long),
+                ("biPlanes", ctypes.c_ushort),
+                ("biBitCount", ctypes.c_ushort),
+                ("biCompression", ctypes.c_ulong),
+                ("biSizeImage", ctypes.c_ulong),
+                ("biXPelsPerMeter", ctypes.c_long),
+                ("biYPelsPerMeter", ctypes.c_long),
+                ("biClrUsed", ctypes.c_ulong),
+                ("biClrImportant", ctypes.c_ulong)]
+
+class RGBQuad(ctypes.Structure):
+    _fields_ = [("rgbBlue", ctypes.c_byte),
+                ("rgbGreen", ctypes.c_byte),
+                ("rgbRed", ctypes.c_byte),
+                ("rgbReserved", ctypes.c_byte)]
+
+class BitmapInfo(ctypes.Structure):
+    _fields_ = [("bmiHeader", BitmapInfoHeader),
+                ("bmiColors", RGBQuad)]
+
+def get_window_rect(hwnd):
+    rect = ctypes.wintypes.RECT()
+    user32.GetWindowRect(hwnd, ctypes.byref(rect))
+    return rect.left, rect.top, rect.right, rect.bottom
 
 class Roblox:
     CLASS_NAMES = {
@@ -46,76 +76,73 @@ class Roblox:
 
     def find_roblox(self, retries=20):
         for _ in range(retries):
-            if hwnd := win32gui.FindWindow(self._name, "Roblox"):
+            if hwnd := user32.FindWindowW(self._name, "Roblox"):
                 self._hwnd = hwnd
                 break
             time.sleep(1)
         else:
             raise Exception("Could not find roblox")
-    
+
     def close_roblox(self):
-        win32gui.PostMessage(self._hwnd, win32con.WM_CLOSE, 0, 0)
+        user32.PostMessageW(self._hwnd, WM_CLOSE, 0, 0)
 
     def is_crashed(self):
         if self._hwnd == 0:
             return False
         
         if self._name == "WINDOWSCLIENT":
-            if (win := win32gui.FindWindow(None, "Roblox Crash")) != 0:
-                win32gui.PostMessage(win, win32con.WM_CLOSE, 0, 0)
+            if win := user32.FindWindowW(None, "Roblox Crash"):
+                user32.PostMessageW(win, WM_CLOSE, 0, 0)
                 self._hwnd = 0
                 return True
 
-        if not win32gui.IsWindow(self._hwnd):
+        if not user32.IsWindow(self._hwnd):
             self._hwnd = 0
             return True
         else:
             return False
     
     def get_screenshot(self):
-        if not win32gui.IsWindow(self._hwnd):
+        if not user32.IsWindow(self._hwnd):
             return None
-        
-        scaleFactor = ctypes.windll.user32.GetDpiForWindow(self._hwnd) / 96.0
 
-        left, top, right, bot = win32gui.GetWindowRect(self._hwnd)
+        scaleFactor = user32.GetDpiForWindow(self._hwnd) / 96.0
+
+        left, top, right, bot = get_window_rect(self._hwnd)
         unscaled_w, unscaled_h = right - left, bot - top
         scaled_w, scaled_h = round(unscaled_w * scaleFactor), round(unscaled_h * scaleFactor)
 
-        for _ in range(3):
-            try:
-                hwndDC = win32gui.GetWindowDC(self._hwnd)
-                mfcDC = win32ui.CreateDCFromHandle(hwndDC)
-                saveDC = mfcDC.CreateCompatibleDC()
+        hwndDC = user32.GetWindowDC(self._hwnd)
+        mfcDC = gdi32.CreateCompatibleDC(hwndDC)
 
-                saveBitMap = win32ui.CreateBitmap()
-                saveBitMap.CreateCompatibleBitmap(mfcDC, scaled_w, scaled_h)
-                saveDC.SelectObject(saveBitMap)
+        saveBitMap = gdi32.CreateCompatibleBitmap(hwndDC, scaled_w, scaled_h)
+        gdi32.SelectObject(mfcDC, saveBitMap)
 
-                match self._name:
-                    case "WINDOWSCLIENT":
-                        saveDC.BitBlt((0, 0), (scaled_w, scaled_h), mfcDC, (0, 0), win32con.SRCCOPY)
-                    case "ApplicationFrameWindow":
-                        ctypes.windll.user32.PrintWindow(self._hwnd, saveDC.GetSafeHdc(), 2)
-                    case _:
-                        return None
+        match self._name:
+            case "WINDOWSCLIENT":
+                gdi32.BitBlt(mfcDC, 0, 0, scaled_w, scaled_h, hwndDC, 0, 0, SRCCOPY)
+            case "ApplicationFrameWindow":
+                user32.PrintWindow(self._hwnd, mfcDC, 2)
+            case _:
+                return None
 
-                bmpinfo = saveBitMap.GetInfo()
-                bmpstr = saveBitMap.GetBitmapBits(True)
+        bmpinfo = BitmapInfo()
+        bmpinfo.bmiHeader.biSize = ctypes.sizeof(BitmapInfoHeader)
+        bmpinfo.bmiHeader.biWidth = scaled_w
+        bmpinfo.bmiHeader.biHeight = -scaled_h
+        bmpinfo.bmiHeader.biPlanes = 1
+        bmpinfo.bmiHeader.biBitCount = 32
+        bmpinfo.bmiHeader.biCompression = 0
 
-                # win32gui.ReleaseDC(self._hwnd, hwndDC)
-                mfcDC.DeleteDC()
-                saveDC.DeleteDC()
-                win32gui.DeleteObject(saveBitMap.GetHandle())
-            except:
-                continue
-            break
-        else:
-            self._hwnd = 0
-            raise Exception("Could not grab screenshot, crashed?")
+        bmpstr = ctypes.create_string_buffer(scaled_w * scaled_h * 4)
+        gdi32.GetDIBits(mfcDC, saveBitMap, 0, scaled_h, bmpstr, ctypes.byref(bmpinfo), 0)
 
-        img = np.frombuffer(bmpstr, dtype=np.uint8)
-        img.shape = (bmpinfo['bmHeight'], bmpinfo['bmWidth'], 4)
+        gdi32.DeleteObject(saveBitMap)
+        gdi32.DeleteDC(mfcDC)
+        user32.ReleaseDC(self._hwnd, hwndDC)
+
+        img = np.frombuffer(bmpstr.raw, dtype=np.uint8)
+        img.shape = (scaled_h, scaled_w, 4)
         img = img[:, :, :3]
 
         img = cv2.resize(img, (unscaled_w, unscaled_h), interpolation=cv2.INTER_NEAREST)
@@ -128,7 +155,7 @@ class Roblox:
     @property
     def name(self):
         return self._name
-    
+
     @property
     def hwnd(self):
         return self._hwnd
