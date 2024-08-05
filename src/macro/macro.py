8affb747a612f6c0c714a28ccb335959ce88a6d7
Okmada <adamko.deak@gmail.com>
2024-08-05 10:51:09 +0200

    Add fail_n_times to get_data

--- src/macro/macro.py
+++ src/macro/macro.py
@@ -14,6 +14,7 @@ class Macro:
         self.config = config
 
         self._passes = 0
+        self._fails = 0
         self._phase = 0
 
     def __call__(self, *args, **kwargs) -> bool | Type[Data]:
@@ -22,23 +23,32 @@ class Macro:
 
         assert return_val is not None, "Return value is None"
 
-        if hasattr(func, "ensure"):
-            amount, delay = func.ensure
-        else:
-            amount, delay = 1, 0
+        pass_n, pass_delay = getattr(func, "ensure", (1, None))
+        fail_n, fail_delay, fail_increment = getattr(func, "max_tries", (None, None, None))
 
         if return_val:
             self._passes += 1
+            self._fails = 0
 
-            if self._passes >= amount:
+            if self._passes >= pass_n:
                 self._passes = 0
+                self._fails = 0
 
-                self._phase += 1
-                self._phase %= len(self.steps)
+                self._chnage_phase(1)
             else:
-                time.sleep(delay)
+                time.sleep(pass_delay)
         else:
             self._passes = 0
+            self._fails += 1
+
+            if fail_n is not None:
+                if self._fails >= fail_n:
+                    self._passes = 0
+                    self._fails = 0
+
+                    self._chnage_phase(fail_increment)
+                else:
+                    time.sleep(fail_delay)
 
         return return_val
 
@@ -49,8 +59,18 @@ class Macro:
     def restart(self) -> None:
         self._phase = 0
 
+    def _chnage_phase(self, increment: int) -> None:
+        self._phase += increment
+        self._phase %= len(self.steps)
+
 def ensure_n_times(n: int, delay: float):
     def wrapper(func):
         func.ensure = (n, delay)
+        return func
+    return wrapper
+
+def fail_n_times(n: int, delay: float, steps_return: int):
+    def wrapper(func):
+        func.max_tries = (n, delay, steps_return)
         return func
     return wrapper
