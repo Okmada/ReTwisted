0ffbafab695b9a46e48f9448192269988e415f57
Okmada <83232017+Okmada@users.noreply.github.com>
2025-05-06 15:10:02 +0200

    Added prompt to ODR

--- src/odr.py
+++ src/odr.py
@@ -29,14 +29,25 @@ class ODR(metaclass=Singleton):
         npSamples = np.array(self.samples).astype(np.float32)
         self.model.train(samples=npSamples[:, :-1], responses=npSamples[:, -1:], layout=cv2.ml.ROW_SAMPLE)
 
+    def prompt(self, sample: np.ndarray):
+        windowName = f"Sample #{len(self.samples)}"
+        
+        cv2.imshow(windowName, sample)
+
+        while (key := (cv2.waitKey(0) - ord("0"))) < 0 or key > 9:
+            continue
+        self.samples.append(self.prepare_for_odr(sample).flatten().tolist() + [key])
+
+        cv2.destroyWindow(windowName)
+
     def detect(self, sample: np.ndarray) -> int:
         prepped = self.prepare_for_odr(sample)
-        retval, results, neigh_resp, dists = self.model.findNearest(prepped, k=1)
+        reshape = prepped.reshape((1,100)).astype(np.float32)
+        retval, results, neigh_resp, dists = self.model.findNearest(reshape, k=1)
         # print(int(results[0][0]))
         return int(results[0][0])
 
     def prepare_for_odr(self, image: np.ndarray) -> np.ndarray:
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        resize = cv2.resize(gray, (10, 10))
-        reshape = resize.reshape((1,100))
-        return reshape.astype(np.float32)
+        if len(image.shape) == 3:
+            cv2.cvtColor(image, cv2.COLOR_BGR2GRAY, image)
+        return cv2.resize(image, (10, 10))
