7f2207dc022617c1cdca7ab4976d2eb58b1e5c7f
Okmada <83232017+Okmada@users.noreply.github.com>
2025-12-24 17:35:17 +0100

    Add Twisted event macro

--- src/macro/macros.py
+++ src/macro/macros.py
@@ -1,9 +1,11 @@
 from macro.helicitymacro.helicitymacro import HelicityMacro
 from macro.twistedmacro.twistedmacro_1_19_1 import TwistedMacro_1_19_1
 from macro.twistedmacro.twistedmacro_latest import TwistedMacro_latest
+from macro.twistedmacro.twistedmacro_event import TwistedMacro_event
 
 Macros = {m.__name__: m for m in [
     TwistedMacro_latest,
+    TwistedMacro_event,
     TwistedMacro_1_19_1,
     HelicityMacro,
 ]}
---
+++ src/macro/twistedmacro/twistedmacro_event.py
@@ -0,0 +1,4 @@
+from macro.twistedmacro.twistedmacro_latest import TwistedMacro_latest
+
+class TwistedMacro_event(TwistedMacro_latest):
+    PLACE_ID = "140605418592999"
